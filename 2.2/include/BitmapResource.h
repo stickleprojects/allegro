a02f9ed2a026095a1b2ad23dc429481ad1d641dd
kieron <kieron.wray@gmail.com>
2019-07-09 21:46:39 +0100

    added 2.2

---
+++ 2.2/include/BitmapResource.h
@@ -0,0 +1,27 @@
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_image.h>
+#include <string>
+
+#ifndef BITMAP_RESOURCE_H
+#define BITMAP_RESOURCE_H
+
+
+#define debug_print(fmt, ...) \
+            do { if (DEBUG) fprintf(stderr, fmt, __VA_ARGS__); } while (0)
+
+class BitmapResource {
+    private: 
+        std::string RelativePath; 
+        ALLEGRO_BITMAP *bmp;      
+    public:
+        operator ALLEGRO_BITMAP*()  {
+            return bmp;
+        }
+        
+        ~BitmapResource();
+        BitmapResource(std::string relativePath, ALLEGRO_BITMAP *bitmap);
+        ALLEGRO_BITMAP *GetBitmap();
+       std::string GetRelativePath();
+};
+
+#endif
