0ffa1344338abd069c988f8cf8bc68808b26b49a
kieron <kieron.wray@gmail.com>
2019-07-27 12:43:29 +0100

    animations implemented

---
+++ install-allegro.sh
@@ -0,0 +1,9 @@
+# taken from https://wiki.allegro.cc/index.php?title=Install_Allegro5_From_Git/Linux/Debian
+
+sudo add-apt-repository ppa:allegro/5.2
+
+sudo apt-get update
+sudo apt-get install liballegro5-dev
+
+
+export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig/
