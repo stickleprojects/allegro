78daac49580107fe1b260f37cc258cbff79335e2
kieron <kieron.wray@gmail.com>
2019-07-28 10:44:23 +0100

    updated cmake gitignore and added main.h

--- game1/src/main.cpp
+++ game1/src/main.cpp
@@ -1,41 +1,4 @@
-#include "ResourceManager.h"
-#include "Sprite.h"
-#include "AnimatedSprite.h"
-#include "AnimationFrame.h"
-#include "SimpleAnimationFrame.h"
-#include <vector>
-#include <string>
-
-
-ALLEGRO_FONT *font = NULL;
-ALLEGRO_TIMER *timer = NULL;
-ALLEGRO_EVENT_QUEUE *queue = NULL;
-ALLEGRO_DISPLAY *disp = NULL;
-ResourceManager *rm = NULL;
-Sprite *player = NULL;
-
-#define SCREENW 1024 * 2 // 640
-#define SCREENH 960 * 2  //480
-#define CAMERA_X 0
-#define CAMERA_Y 0
-#define CAMERA_SCALE 3.0f
-
-#define BACKGROUND_IMAGE "resources/background.png"
-struct POINT
-{
-    int x;
-    int y;
-
-    POINT(int theX, int theY)
-    {
-        this->x = theX;
-        this->y = theY;
-    }
-};
-
-POINT cameraPosition = POINT(0, 0);
-ALLEGRO_TRANSFORM camera;
-float cameraScale = CAMERA_SCALE;
+#include "main.h"
 
 void moveSprite(Sprite *sprite)
 {
@@ -108,8 +71,6 @@ int initResources()
 
     return 0;
 }
-int cameraX = CAMERA_X;
-int cameraY = CAMERA_Y;
 bool updateCamera()
 {
     if (cameraX < 0)
@@ -138,7 +99,6 @@ void DrawSprites()
 
     player->Draw();
 }
-AnimationFrame *first = NULL;
 
 void updateNextPointers(std::vector<AnimationFrame *> tgt)
 {
