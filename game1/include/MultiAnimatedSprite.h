9b3d49c86ffc57acd222e66ced1794b38547cf0e
kieron <kieron.wray@gmail.com>
2019-08-08 18:05:33 +0100

    added stationery support for animations

--- game1/include/MultiAnimatedSprite.h
+++ game1/include/MultiAnimatedSprite.h
@@ -4,16 +4,20 @@
 #include "AnimatedSprite.h"
 #include <map>
 
-class MultiAnimatedSprite : public AnimatedSprite {
+class MultiAnimatedSprite : public AnimatedSprite
+{
 private:
-    std::map<std::string, AnimationFrame*> frames ;
-AnimationFrame* FindAnimationSetById(std::string id);
-public:
-    MultiAnimatedSprite(std::map<std::string,AnimationFrame*> frames, std::string defaultAnimationId);
-    void SetDirection(int x, int y) ;
+    std::map<std::string, AnimationFrame *> frames;
+    AnimationFrame *FindAnimationSetById(std::string id);
+std::string currentAnimationSetId;
+bool playAnimations=false;
+void resetCurrentFrame();
 
+public:
+    MultiAnimatedSprite(std::map<std::string, AnimationFrame *> frames, std::string defaultAnimationId);
+    void SetDirection(int x, int y) override;
+    void Update() override;
+    
 };
 
-
 #endif
-
--- game1/src/MultiAnimatedSprite.cpp
+++ game1/src/MultiAnimatedSprite.cpp
@@ -1,31 +1,69 @@
 #include "MultiAnimatedSprite.h"
 
-
-void MultiAnimatedSprite::SetDirection(int x, int y) {
-    if(x>0) {
-        currentFrame=FindAnimationSetById("right");
-    } else if (x<0) {
-        currentFrame=FindAnimationSetById("left");
-    } else if (y>0) {
-        currentFrame=FindAnimationSetById("down");
-    } else if (y<0) {
-        currentFrame=FindAnimationSetById("up");
+void MultiAnimatedSprite::Update()
+{
+    // do nowt
+    if (playAnimations)
+    {
+        AnimatedSprite::Update();
+    }
+    else
+    {
+        resetCurrentFrame();
+    }
+}
+void MultiAnimatedSprite::resetCurrentFrame()
+{
+    currentFrame = FindAnimationSetById(currentAnimationSetId);
+}
+void MultiAnimatedSprite::SetDirection(int x, int y)
+{
+    if (x > 0)
+    {
+        currentFrame = FindAnimationSetById("right");
+        currentAnimationSetId = "right";
+    }
+    else if (x < 0)
+    {
+        currentFrame = FindAnimationSetById("left");
+        currentAnimationSetId = "left";
+    }
+    else if (y > 0)
+    {
+        currentFrame = FindAnimationSetById("down");
+        currentAnimationSetId = "down";
+    }
+    else if (y < 0)
+    {
+        currentFrame = FindAnimationSetById("up");
+        currentAnimationSetId = "up";
+    }
+    if (x == 0 && y == 0)
+    {
+        playAnimations = false;
+    }
+    else
+    {
+        playAnimations = true;
     }
 }
 
-AnimationFrame* MultiAnimatedSprite::FindAnimationSetById(std::string id) {
-    for(auto it=this->frames.begin(); it!=this->frames.end(); ++it) {
-        if(it->first == id) {
+AnimationFrame *MultiAnimatedSprite::FindAnimationSetById(std::string id)
+{
+    for (auto it = this->frames.begin(); it != this->frames.end(); ++it)
+    {
+        if (it->first == id)
+        {
             return (it->second);
         }
     }
     return NULL;
 }
-MultiAnimatedSprite::MultiAnimatedSprite(std::map<std::string,AnimationFrame*> frames, std::string defaultAnimationId):AnimatedSprite(NULL) {
-    
+MultiAnimatedSprite::MultiAnimatedSprite(std::map<std::string, AnimationFrame *> frames, std::string defaultAnimationId) : AnimatedSprite(NULL)
+{
+
     this->frames = frames;
 
-    firstFrame=FindAnimationSetById(defaultAnimationId);
-    currentFrame=firstFrame;
-
+    firstFrame = FindAnimationSetById(defaultAnimationId);
+    currentFrame = firstFrame;
 }
