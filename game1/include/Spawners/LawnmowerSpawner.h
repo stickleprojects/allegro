29e74b0c20fe4de611992e5e4e1d0cf6d4ca04d2
kieron <kieron.wray@gmail.com>
2019-09-16 14:30:36 +0100

    added creator callbacks to lawnmowerspaner

--- game1/include/Spawners/LawnmowerSpawner.h
+++ game1/include/Spawners/LawnmowerSpawner.h
@@ -1,11 +1,34 @@
 #ifndef SPAWNERS_LAWNMOWERSPAWNER_H_
 #define SPAWNERS_LAWNMOWERSPAWNER_H_
 
+#include "Sprite.h"
 #include "Spawner.h"
 
+#include <functional>
+
+typedef std::function<Sprite*(int,int,int)> NPCCreatorFunction;
+
+struct LANE {
+    int xinc;
+    int ypos;
+    int xpos;
+
+    LANE(int xinc,int xpos, int ypos) {
+        this->xinc = xinc;
+        this->ypos = ypos;
+        this->xpos = xpos;
+    }
+};
+
 class LawnmowerSpawner:public Spawner {
+    private:
+        Sprite* CreateLawnmowerNPC(int laneIdx);
+        LANE lanes[3];
+        void initLanes(int minx, int maxx);
+        NPCCreatorFunction npcCreatorFunction;
+        
     public:
-        LawnmowerSpawner();
+        LawnmowerSpawner(int minx, int maxx, NPCCreatorFunction npcCreatorFunction);
         void Update() override;
         void Spawn() override;
         
--- game1/src/Spawners/LawnmowerSpawner.cpp
+++ game1/src/Spawners/LawnmowerSpawner.cpp
@@ -1,7 +1,22 @@
 #include "Spawners/LawnmowerSpawner.h"
 
-LawnmowerSpawner::LawnmowerSpawner() {
+LawnmowerSpawner::LawnmowerSpawner(int min_x, int max_x, NPCCreatorFunction npcCreatorFunction) {
+    initLanes(min_x, max_x);
+    this->npcCreatorFunction = npcCreatorFunction;
+}
 
+void LawnmowerSpawner::initLanes(int min_x, int max_x)  {
+    lanes[0] = LANE(1,min_x, 320);
+    lanes[1] = LANE(-1,max_x, 364);
+    lanes[2] = LANE(1,min_x, 400);
+
+}
+Sprite *LawnmowerSpawner::CreateLawnmowerNPC(int laneIdx) {
+    int x = lanes[laneIdx].xpos;
+    int directionX = lanes[laneIdx].xinc;
+    int y = lanes[laneIdx].ypos;
+
+    return npcCreatorFunction(x, y, directionX);
 }
 
 void LawnmowerSpawner::Update() {
